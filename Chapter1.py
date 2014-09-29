9e5b868f21b00727df1855806d532e567418663b
tom20314 <tom20314@gmail.com>
2014-09-29 15:15:28 +0800

    work
    work

--- Chapter1.py
+++ Chapter1.py
@@ -1,2 +1,4 @@
-print ("我的第一隻Python程式")
-	
+#print ("我的第一隻Python程式")
+
+name = input("What's your name ?\n" )	
+print ('Hello,%s.'%name )
---
+++ test.py
@@ -0,0 +1,8 @@
+#test
+
+
+def f(x):
+      x = x**2+x+1
+      return x
+
+print (f(3))
